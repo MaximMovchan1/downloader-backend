e33141003cd9d699abaab75062dec21c1d929380
Maxim Movchan <Maximush555@gmail.com>
2024-04-28 08:17:34 +0300

    add cors support

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const puppeteer = require("puppeteer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/api/download-instagram-reel", async (req, res) => {
   try {
